66bea5bccd37742d315a542d8f050c1600323886
anunyaOrange <anunyaOrange@proton.me>
2025-09-16 11:41:16 +0700

    try pos

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const runner = require('./test-runner.js');
 
 const app = express();
 
-app.use(helmet.noSniff());
-app.use(helmet.xssFilter());
-app.use(helmet.noCache());
-app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
+// app.use(helmet.noSniff());
+// app.use(helmet.xssFilter());
+// app.use(helmet.noCache());
+// app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
@@ -24,6 +24,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //For FCC testing purposes and enables user to connect from outside the hosting platform
 app.use(cors({origin: '*'})); 
+
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
+app.use(helmet.noCache());
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
 
 // Index page (static HTML)
 app.route('/')
